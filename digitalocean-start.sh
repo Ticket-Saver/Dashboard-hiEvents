faad80f8f8b69c7bf73b3d3595f995642be63205
JorgeOchoa <jeob2046@gmail.com>
2025-02-11 22:25:53 -0500

    Restoring

--- digitalocean-start.sh
+++ digitalocean-start.sh
@@ -1,18 +1,7 @@
-#!/bin/bash
-
-# Asegurarse de que el script tenga permisos de ejecución
-chmod +x /digitalocean-start.sh
 
 export VITE_FRONTEND_URL=${APP_FRONTEND_URL:-"/"}
 
 echo "Starting with the following environment variables:"
 echo "VITE_FRONTEND_URL=${VITE_FRONTEND_URL}"
 
-cd /app/backend
-
-# Asegurarse de que la base de datos esté lista
-php artisan migrate:status
-# Ejecutar migraciones pendientes
-php artisan migrate --force
-
-exec /startup.sh
+exec /app/backend/startup.sh
