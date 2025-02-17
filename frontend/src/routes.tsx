eb9d23de1793619d53989355a2befd5ae6c6235b
JorgeOchoa <jeob2046@gmail.com>
2025-02-17 11:14:24 -0500

    Remove tickets route configuration

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,1 @@
-import { Tickets } from "./components/routes/event/tickets";
-
-// ... otras importaciones
-
-export const routes = [
-  {
-    path: "/manage/event/:eventId/tickets",
-    element: <Tickets />,
-  },
-  // ... otras rutas
-]; 
+ 
