ed76ff8a89501521aab13f577910743210cef15b
JorgeOchoa <jeob2046@gmail.com>
2025-02-24 09:22:27 -0500

    Update CORS configuration and add image caching headers

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,7 +15,12 @@ return [
     |
     */
 
-    'paths' => ['*', 'api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*', 
+        'sanctum/csrf-cookie',
+        'storage/*',
+        '*'
+    ],
 
     'allowed_methods' => ['*'],
 
